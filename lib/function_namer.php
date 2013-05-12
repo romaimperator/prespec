8b5f36132d00f1a05f33ead8d74a06e87eaa1879
romaimperator <romaimperator@gmail.com>
2013-05-12 02:28:00 -0500

    Adding basic support for it blocks

--- lib/function_namer.php
+++ lib/function_namer.php
@@ -1,10 +1,42 @@
 <?php
 class FunctionNamer {
-  function replace($spec_string) {
+  function replace($spec) {
+    $spec = FunctionNamer::name_describe($spec);
+    $spec = FunctionNamer::name_all_its($spec);
+    return $spec;
+  }
+
+  function slug($string) {
+    return preg_replace("/(\w) (\w|['\"])/", "$1_$2", $string);
+  }
+
+  function name_it($string) {
+    return preg_replace(
+      "/([ \t]*)(it_)['\"]([^'\"]+)['\"]/",
+      "$1function $2$3()",
+      $string);
+  }
+
+  private function name_describe($spec) {
     return preg_replace(
       "/describe ([A-Za-z]+)\b/",
       "function describe_$1()",
-      $spec_string);
+      $spec);
+  }
+
+  private function name_all_its($spec) {
+    $spec = FunctionNamer::slug_its($spec);
+    return $spec;
+  }
+
+  private function slug_its($spec) {
+    return preg_replace_callback(
+      "/[ \t]*(it ['\"][^\n]+['\"])/",
+      function ($matches) {
+        return FunctionNamer::name_it(FunctionNamer::slug($matches[0]));
+      },
+      $spec);
   }
 }
 ?>
+
--- spec/spec.spec
+++ spec/spec.spec
@@ -2,5 +2,8 @@ describe Spec do
   puts 'hi'
   puts 'whazzup?'
   it 'should be able to have it blocks'
+  puts "it doesn't change this"
+  it 'should handle having a block' do
+  end
 end
 
